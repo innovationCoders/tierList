044f7d6d6a18eba405cd60dcb8ed4d8497f5af8e
Inno Melkonjan <monkeytoffy@gmail.com>
2024-08-20 11:48:13 +0200

    add drag and drop function, with a bug

--- index.js
+++ index.js
@@ -4,28 +4,89 @@ const $$ = el => document.querySelectorAll(el);
 const imageInput = $('#image-input');
 const itemsSection = $('#selector-items');
 
+function createItem(src) {
+    const imgElement = document.createElement('img');
+    imgElement.draggable = true;
+    imgElement.src = src;
+    imgElement.className = 'item-image';
+
+    imgElement.addEventListener("dragstart", handleDragStart)
+    imgElement.addEventListener("dragend", handleDragEnd)
+
+    itemsSection.appendChild(imgElement);
+    return imgElement
+}
+
 imageInput.addEventListener('change', (event) => {
     const [file] = event.target.files;
-    console.log('Archivo seleccionado:', file);
 
     if (file) {
         const reader = new FileReader();
-        console.log('FileReader creado');
 
         reader.onload = (eventReader) => {
-            console.log('FileReader onload ejecutado');
-            console.log('Resultado del FileReader:', eventReader.target.result);
-
-            const imgElement = document.createElement('img');
-            imgElement.src = eventReader.target.result;
-            imgElement.className = 'item-image';
-            itemsSection.appendChild(imgElement);
-            console.log('Imagen añadida al DOM');
+            createItem(eventReader.target.result)  
         };
 
         reader.readAsDataURL(file);
-        console.log('readAsDataURL llamado');
-    } else {
-        console.error('No se seleccionó ningún archivo o el archivo no es válido.');
+       
     }
 });
+
+let draggedElement = null;
+let sourceContainer = null;
+
+const rows = $$(".tier .row")
+
+rows.forEach(row => {
+    row.addEventListener("drop", handleDrop)
+    row.addEventListener("dragover", handleDragOver)
+    row.addEventListener("dragleave", handleDragLeave)
+})
+
+function handleDrop(event) {
+    event.preventDefault()
+    const { currentTarget, dataTransfer } = event;
+    console.log(dataTransfer.getData("text/plain"));
+
+    if (sourceContainer && draggedElement) {
+        sourceContainer.removeChild(draggedElement)
+    }
+
+    if (draggedElement) {
+        const src = dataTransfer.getData("text/plain")
+        const imgElement = createItem(src)
+        currentTarget.appendChild(imgElement)
+    }
+    currentTarget.classList.remove("drag-over")
+}
+function handleDragOver(event) {
+    event.preventDefault()
+    const { currentTarget, dataTransfer } = event;
+
+    if(sourceContainer === currentTarget) return
+    
+    currentTarget.classList.add("drag-over")
+    
+}
+function handleDragLeave(event) {
+    event.preventDefault()
+
+    const { currentTarget } = event
+    currentTarget.classList.remove("drag-over")
+    
+}
+
+
+function handleDragStart(event) {
+    console.log("drag start", event.target);
+    draggedElement = event.target
+    sourceContainer = draggedElement.parentNode
+    event.dataTransfer.setData("text/plain", draggedElement.src)
+}
+ 
+function handleDragEnd(event) {
+    console.log("drag end", event.target);
+    draggedElement = null;
+    sourceContainer = null;
+    
+}
